074584b70c3ffe4033666f25a490a7b12d963250
OMENDRA SINGH <146812984+OMENDRA1604@users.noreply.github.com>
2024-12-30 12:35:00 +0000

    date in js

--- 01_Basics/07_datesinJs.js
+++ 01_Basics/07_datesinJs.js
@@ -4,6 +4,23 @@ let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
+
+/*console.log(myDate)
+
+console.log(myDate.toString());
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toLocaleString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleTimeString());
+
+*/
+
 // console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
